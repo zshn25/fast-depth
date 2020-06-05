6e7b951552cdf409c5de7ce696a87e8c120f9143
zshn25 <zshn25@gmail.com>
2020-06-05 10:36:02 +0200

    Trying to upgrade with MobileNet v2

--- models.py
+++ models.py
@@ -49,13 +49,20 @@ def weights_init(m):
         m.weight.data.fill_(1)
         m.bias.data.zero_()
 
-def conv(in_channels, out_channels, kernel_size):
+def conv(in_channels, out_channels, kernel_size, relu6=False):
     padding = (kernel_size-1) // 2
     assert 2*padding == kernel_size-1, "parameters incorrect. kernel={}, padding={}".format(kernel_size, padding)
-    return nn.Sequential(
+    if not relu6:    
+        return nn.Sequential(
+            nn.Conv2d(in_channels,out_channels,kernel_size,stride=1,padding=padding,bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+    else:
+        return nn.Sequential(
           nn.Conv2d(in_channels,out_channels,kernel_size,stride=1,padding=padding,bias=False),
           nn.BatchNorm2d(out_channels),
-          nn.ReLU(inplace=True),
+          nn.ReLU6(inplace=True),
         )
 
 def depthwise(in_channels, kernel_size):
@@ -652,12 +659,95 @@ class ResNetSkipConcat(nn.Module):
         return y
 
 class MobileNetSkipAdd(nn.Module):
+    def __init__(self, output_size, pretrained=True,  
+                 pretrained_path = os.path.join('imagenet', 
+                 'results', 'imagenet.arch=mobilenet.lr=0.1.bs=256',
+                 'model_best.pth.tar')):
+
+        super(MobileNetSkipAdd, self).__init__()
+        self.output_size = output_size
+        mobilenet = imagenet.mobilenet.MobileNet()
+        if pretrained:
+            #pretrained_path = os.path.join('imagenet', 'results', 'imagenet.arch=mobilenet.lr=0.1.bs=256', 'model_best.pth.tar')
+            checkpoint = torch.load(pretrained_path)
+            state_dict = checkpoint['state_dict']
+
+            from collections import OrderedDict
+            new_state_dict = OrderedDict()
+            for k, v in state_dict.items():
+                name = k[7:] # remove `module.`
+                new_state_dict[name] = v
+            mobilenet.load_state_dict(new_state_dict)
+        else:
+            mobilenet.apply(weights_init)
+
+        for i in range(14):
+            setattr( self, 'conv{}'.format(i), mobilenet.model[i])
+
+        kernel_size = 5
+        # self.decode_conv1 = conv(1024, 512, kernel_size)
+        # self.decode_conv2 = conv(512, 256, kernel_size)
+        # self.decode_conv3 = conv(256, 128, kernel_size)
+        # self.decode_conv4 = conv(128, 64, kernel_size)
+        # self.decode_conv5 = conv(64, 32, kernel_size)
+        self.decode_conv1 = nn.Sequential(
+            depthwise(1024, kernel_size),
+            pointwise(1024, 512))
+        self.decode_conv2 = nn.Sequential(
+            depthwise(512, kernel_size),
+            pointwise(512, 256))
+        self.decode_conv3 = nn.Sequential(
+            depthwise(256, kernel_size),
+            pointwise(256, 128))
+        self.decode_conv4 = nn.Sequential(
+            depthwise(128, kernel_size),
+            pointwise(128, 64))
+        self.decode_conv5 = nn.Sequential(
+            depthwise(64, kernel_size),
+            pointwise(64, 32))
+        self.decode_conv6 = pointwise(32, 1)
+        weights_init(self.decode_conv1)
+        weights_init(self.decode_conv2)
+        weights_init(self.decode_conv3)
+        weights_init(self.decode_conv4)
+        weights_init(self.decode_conv5)
+        weights_init(self.decode_conv6)
+
+    def forward(self, x):
+        # skip connections: dec4: enc1
+        # dec 3: enc2 or enc3
+        # dec 2: enc4 or enc5
+        for i in range(14):
+            layer = getattr(self, 'conv{}'.format(i))
+            x = layer(x)
+            # print("{}: {}".format(i, x.size()))
+            if i==1:
+                x1 = x
+            elif i==3:
+                x2 = x
+            elif i==5:
+                x3 = x
+        for i in range(1,6):
+            layer = getattr(self, 'decode_conv{}'.format(i))
+            x = layer(x)
+            x = F.interpolate(x, scale_factor=2, mode='nearest')
+            if i==4:
+                x = x + x1
+            elif i==3:
+                x = x + x2
+            elif i==2:
+                x = x + x3
+            print("{}: {}".format(i, x.size()))
+        x = self.decode_conv6(x)
+        return x
+
+class MobileNetSkipAddMultiScale(nn.Module):
     def __init__(self, output_size, pretrained=True, 
                  pretrained_path = os.path.join('imagenet', 
                  'results', 'imagenet.arch=mobilenet.lr=0.1.bs=256',
                  'model_best.pth.tar'), scales = range(4)):
 
-        super(MobileNetSkipAdd, self).__init__()
+        super(MobileNetSkipAddMultiScale, self).__init__()
         self.output_size = output_size
         mobilenet = imagenet.mobilenet.MobileNet()
         if pretrained:
